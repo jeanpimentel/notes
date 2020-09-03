262e28a8652ee2aa1fa6c2dca3fab35ee44cc640
Jean Pimentel <contato@jeanpimentel.com.br>
2020-09-03 20:24:45 -0300

    Black formatting

--- generate_readme.py
+++ generate_readme.py
@@ -2,9 +2,10 @@
 
 import os
 import re
-from dataclasses import dataclass
 from operator import attrgetter
 from typing import List, Optional
+
+from dataclasses import dataclass
 
 
 @dataclass
@@ -17,29 +18,29 @@ class Note:
 
 def get_title(filepath: str) -> str:
     with open(filepath) as note:
-        return next(note).strip('#').strip()
+        return next(note).strip("#").strip()
 
 
 def get_date(file: str) -> Optional[str]:
-    match = re.search(r'(\d{4}-\d{2}-\d{2})', file)
+    match = re.search(r"(\d{4}-\d{2}-\d{2})", file)
     if match:
         return match.group(0)
 
-    return ''
+    return ""
 
 
 def get_notes() -> List[Note]:
     results = []
 
     for root, dirs, files in os.walk("."):
-        if root == '.':
+        if root == ".":
             for dir_ in list(dirs):
-                if dir_.startswith('.'):
+                if dir_.startswith("."):
                     dirs.remove(dir_)
             continue
 
         for file in files:
-            if not file.endswith('.md'):
+            if not file.endswith(".md"):
                 continue
 
             basedir = os.path.basename(root)
@@ -58,30 +59,30 @@ def get_notes() -> List[Note]:
 
 
 def sort_notes(notes: List[Note]) -> List[Note]:
-    title_asc = sorted(notes, key=attrgetter('title'))
-    created_at_desc = sorted(title_asc, key=attrgetter('created_at'), reverse=True)
-    category_asc = sorted(created_at_desc, key=attrgetter('category'))
+    title_asc = sorted(notes, key=attrgetter("title"))
+    created_at_desc = sorted(title_asc, key=attrgetter("created_at"), reverse=True)
+    category_asc = sorted(created_at_desc, key=attrgetter("category"))
     return category_asc
 
 
 def generate_header() -> str:
-    with open('.notes/header.md', 'r') as content_file:
+    with open(".notes/header.md", "r") as content_file:
         return content_file.read()
 
 
 def generate_category(category: str) -> str:
-    return f'\n### {category}\n\n'
+    return f"\n### {category}\n\n"
 
 
 def generate_item(note: Note) -> str:
     if note.created_at:
-        return f'- [{note.title.lower()}]({note.filepath}) - {note.created_at}\n'
+        return f"- [{note.title.lower()}]({note.filepath}) - {note.created_at}\n"
 
-    return f'- [{note.title.lower()}]({note.filepath})\n'
+    return f"- [{note.title.lower()}]({note.filepath})\n"
 
 
 def generate_footer() -> str:
-    with open('.notes/footer.md', 'r') as content_file:
+    with open(".notes/footer.md", "r") as content_file:
         return content_file.read()
 
 
@@ -105,9 +106,9 @@ def main():
     notes = sort_notes(get_notes())
 
     content = generate_readme_content(notes)
-    with open('README.md', 'w') as readme:
+    with open("README.md", "w") as readme:
         readme.write(content)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
